7a5d1877804cfbd6db5f82713acb764f791df3c9
eososlinux <eososlinux@gmail.com>
2026-01-06 13:09:05 -0300

    update rust_analyzer

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -1,23 +1,25 @@
 return {
   cmd = { "rust-analyzer" },
-  filetypes = { "rust" },
 
-  root_dir = function(bufnr)
-    return vim.fs.root(bufnr, {
-      "Cargo.toml",
-      "rust-project.json",
-      ".git",
-    }) or vim.fn.getcwd()
-  end,
-
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = {
-        allFeatures = true,
-      },
-      checkOnSave = {
-        command = "clippy",
-      },
-    },
-  },
+--   root_dir = function()
+--     return vim.fs.root(0, {
+--       "Cargo.toml",
+--       "rust-project.json",
+--       ".git",
+--     }) or vim.loop.cwd()
+--   end,
+--
+--   settings = {
+--     ["rust-analyzer"] = {
+--       cargo = {
+--         allFeatures = true,
+--       },
+--     checkOnSave = {
+--       command = "clippy",
+--     },
+--     proMacro = {
+--       enable = true,
+--     },
+--   },
+-- },
 }
