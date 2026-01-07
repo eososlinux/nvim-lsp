0684244511ffb7626759a5412f8be14a28a896b3
eososlinux <eososlinux@gmail.com>
2026-01-07 19:16:23 -0300

    add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = true,                                         -- ← importante: carga solo cuando lualine lo necesite
+  event = { "VimEnter", "BufReadPost", "BufNewFile" }, -- carga temprana pero segura
+  config = function()
+    local lazy_status = require("lazy.status")         -- ahora SÍ está disponible
+
+    require("lualine").setup({
+      options = {
+        -- theme = "auto",
+        theme = "tokyonight",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "dashboard", "alpha" },
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = {
+          "location",
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            function()
+              return " " .. lazy_status.stats().loaded .. "/" .. lazy_status.stats().total
+            end,
+            cond = lazy_status.has_updates or lazy_status.stats().pending > 0,
+            color = { fg = "#a9a1e1" },
+          },
+        },
+      },
+    })
+  end,
+}
